26a3cae0dac4f354f2b25ddf2d8ca3206a63bc1b
Vsevolod Okhrin <vokhrin@ozon.ru>
2023-11-04 17:04:33 +0700

    service2service full

--- internal/app/gateway/service.go
+++ internal/app/gateway/service.go
@@ -2,6 +2,7 @@ package gateway
 
 import (
 	"context"
+	"io"
 
 	pb "github.com/route256/workshop-8/pkg/gateway"
 	pb_messages "github.com/route256/workshop-8/pkg/messages"
@@ -29,4 +30,102 @@ func (i *Implementation) GetMessagesSummary(ctx context.Context, empt *emptypb.E
 	return &pb.MessagesSummary{
 		Count: summary.GetCount(),
 	}, nil
+}
+
+func (i *Implementation) PullMessages(ctx context.Context, _ *emptypb.Empty) (*pb.PullMessagesResponse, error) {
+	stream, err := i.messages.PullMessages(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, err
+	}
+
+	resp := pb.PullMessagesResponse{}
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		resp.Messages = append(resp.Messages, &pb.Message{
+			Ts:     msg.Ts,
+			Text:   msg.Text,
+			Author: msg.Author,
+		})
+	}
+
+	return &resp, nil
+}
+
+func (i *Implementation) PushMessages(ctx context.Context, req *pb.PushMessagesRequest) (*pb.PushMessagesRequestResponse, error) {
+	stream, err := i.messages.PushMessages(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, message := range req.GetMessages() {
+		stream.Send(&pb_messages.Message{
+			Ts:     message.Ts,
+			Text:   message.Text,
+			Author: message.Author,
+		})
+	}
+
+	summary, err := stream.CloseAndRecv()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.PushMessagesRequestResponse{
+		Summary: &pb.MessagesSummary{
+			Count: summary.GetCount(),
+		},
+	}, nil
+}
+
+func (i *Implementation) ExchangeMessages(ctx context.Context, req *pb.ExchangeMessagesRequest) (*pb.ExchangeMessagesResponse, error) {
+	stream, err := i.messages.ExchangeMessages(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp pb.ExchangeMessagesResponse
+	var respErr error
+
+	waitCh := make(chan struct{})
+	go func() {
+		for {
+			message, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					close(waitCh)
+					return
+				}
+				respErr = err
+				return
+			}
+
+			resp.Messages = append(resp.Messages,
+				&pb.Message{
+					Ts:     message.Ts,
+					Text:   message.Text,
+					Author: message.Author,
+				},
+			)
+		}
+	}()
+
+	for _, message := range req.GetMessages() {
+		stream.Send(&pb_messages.Message{
+			Ts:     message.Ts,
+			Text:   message.Text,
+			Author: message.Author,
+		})
+	}
+
+	if err := stream.CloseSend(); err != nil {
+		return nil, err
+	}
+
+	<-waitCh
+	return &resp, respErr
 }
--- internal/app/messages/service.go
+++ internal/app/messages/service.go
@@ -2,6 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
 
 	"google.golang.org/protobuf/types/known/emptypb"
 
@@ -19,4 +23,77 @@ func New() *Implementation {
 func (i *Implementation) GetMessagesSummary(ctx context.Context, _ *emptypb.Empty) (*pb.MessagesSummary, error) {
 
 	return &pb.MessagesSummary{Count: 42}, nil
+}
+
+func (i *Implementation) PullMessages(_ *emptypb.Empty, stream pb.Messages_PullMessagesServer) error {
+	for _, message := range data {
+		stream.Send(message)
+	}
+	return nil
+}
+
+func (i *Implementation) PushMessages(stream pb.Messages_PushMessagesServer) error {
+	var counter int
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return stream.SendAndClose(&pb.MessagesSummary{
+					Count: uint64(counter),
+				})
+			}
+			return err
+		}
+
+		log.Printf("message %q from %s", msg.Text, msg.Author)
+		counter++
+	}
+}
+
+func (i *Implementation) ExchangeMessages(stream pb.Messages_ExchangeMessagesServer) error {
+	for {
+		message, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+
+		stream.Send(&pb.Message{
+			Ts:     time.Now().Format(time.RFC3339),
+			Text:   fmt.Sprintf("RE: %s", message.Text),
+			Author: "server",
+		})
+
+		log.Printf("message %q from %s", message.Text, message.Author)
+	}
+}
+
+var data = []*pb.Message{
+	{
+		Ts:     time.Now().Format(time.RFC3339),
+		Text:   "Message 1",
+		Author: "server",
+	},
+	{
+		Ts:     time.Now().Format(time.RFC3339),
+		Text:   "Message 2",
+		Author: "server",
+	},
+	{
+		Ts:     time.Now().Format(time.RFC3339),
+		Text:   "Message 3",
+		Author: "server",
+	},
+	{
+		Ts:     time.Now().Format(time.RFC3339),
+		Text:   "Message 4",
+		Author: "server",
+	},
+	{
+		Ts:     time.Now().Format(time.RFC3339),
+		Text:   "Message 5",
+		Author: "server",
+	},
 }
