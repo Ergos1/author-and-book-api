f09f4582bea14042534d6b1267df86b21fa1f8a6
Vsevolod Okhrin <vokhrin@ozon.ru>
2023-11-04 15:18:38 +0700

    gen: messages service

---
+++ cmd/messages/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"log"
+)
+
+func main() {
+	ctx := context.Background()
+
+	if err := run(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context) error {
+
+	return nil
+}
